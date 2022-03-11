06399dc687aafe5120b75a20f7b5974d4ec7c1b2
Annika Grothaus <annika.grothaus@gmx.de>
2022-03-11 11:50:30 +0100

    fixed multiple buttons bug

--- src/presenters/authenticationPresenter.js
+++ src/presenters/authenticationPresenter.js
@@ -1,20 +1,19 @@
-import AuthenticationView from "@/views/authenticationView"
-import firebaseConfig from "../firebaseConfig"
-import uiConfig from "@/firebaseUiConfig"
-import * as firebaseui from "firebaseui"
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
+import AuthenticationView from "@/views/authenticationView";
+import firebaseConfig from "../firebaseConfig";
+import uiConfig from "@/firebaseUiConfig";
+import * as firebaseui from "firebaseui";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 const Authentication = {
-    created(){
-        firebase.initializeApp(firebaseConfig)
-        const auth = firebase.auth()
-        const ui = new firebaseui.auth.AuthUI(auth);
-        ui.start("#auth-container", uiConfig);
-    },
-    render(){
-        return <AuthenticationView/>
-    }
-}
+  created() {
+    firebase.initializeApp(firebaseConfig);
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start("#auth-container", uiConfig);
+  },
+  render() {
+    return <AuthenticationView />;
+  },
+};
 
-export default Authentication
+export default Authentication;
