11d033fdff12d3c5b1bb40281512df85b75e9862
Paloma Dominguez <padosa23@gmail.com>
2022-03-22 10:24:14 +0100

    Finish Welcome Page

--- src/views/welcomePageView.js
+++ src/views/welcomePageView.js
@@ -1,19 +1,16 @@
 function WelcomePageView(props){
     return (
-        <section class="hero">
-        <div class="intro-text">
-            <h1>
-                <span class="hear"> Welcome to </span> <br />
-                <span class="connecting"> Greenchy</span>
-            </h1>
-            <p>
-                An online platform for Smart Sustainability <br />
-                Automate emission calculations in 3 simple steps
-            </p>
-            <a class="btn red" href="#">Learn More</a>
-            <a class="btn blue" href="#">Subscribe</a>
+        <section class="hero is-large is-success">
+        <div class="hero-body">
+          <p class="title">
+            Welcome to Greenchy
+          </p>
+          <p class="subtitle">
+            An online platform for Smart Sustainability <br />
+            Measure your carbon emissions wherever you want to travel
+          </p>
         </div>
-        </section>
+      </section>
     );
 }
 
