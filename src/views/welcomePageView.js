a20419199750d500e07a5db6edde2331d4c31a46
Paloma Dominguez <padosa23@gmail.com>
2022-03-22 09:52:07 +0100

    Welcome Page Text Done. Including buttons?

--- src/views/welcomePageView.js
+++ src/views/welcomePageView.js
@@ -1,5 +1,20 @@
 function WelcomePageView(props){
-    return <div>hello</div>
+    return (
+        <section class="hero">
+        <div class="intro-text">
+            <h1>
+                <span class="hear"> Welcome to </span> <br />
+                <span class="connecting"> Greenchy</span>
+            </h1>
+            <p>
+                An online platform for Smart Sustainability <br />
+                Automate emission calculations in 3 simple steps
+            </p>
+            <a class="btn red" href="#">Learn More</a>
+            <a class="btn blue" href="#">Subscribe</a>
+        </div>
+        </section>
+    );
 }
 
 export default WelcomePageView;
