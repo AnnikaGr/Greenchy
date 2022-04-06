e465e6bca6542899fab107d7415f5140a80a2ccf
Annika Grothaus <annika.grothaus@gmx.de>
2022-04-06 13:34:43 +0200

    constructed welcome screen animation

--- src/presenters/welcomePagePresenter.js
+++ src/presenters/welcomePagePresenter.js
@@ -1,9 +1,13 @@
 import WelcomePageView from "@/views/welcomePageView";
+import animateBike from "../views/animateBike.js";
 
 const WelcomePage = {
-    render() {
-        return <WelcomePageView/>
-    }
-}
+  mounted() {
+    animateBike();
+  },
+  render() {
+    return <WelcomePageView />;
+  },
+};
 
 export default WelcomePage;
