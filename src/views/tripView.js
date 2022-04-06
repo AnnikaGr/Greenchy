2bcc965baa9d9c5f9109abc386cbe216fcbbc227
Simon Langrieger <simon.langrieger@tum.de>
2022-04-06 13:29:30 +0200

    minor view updates

--- src/views/tripView.js
+++ src/views/tripView.js
@@ -7,27 +7,20 @@ function TripView(props) {
     }
     return (
       <div>
-          <h1>Your trip</h1>
-          <div class="card">
-                    <div class="card-header">
-                        <h1>Overall Co2 Emission</h1>
-                    </div>
-                <div class="card-content">
-                    <h1>{props.overallCo2.toFixed(4)}</h1>
+          <div class="box">
+                <h2 class="title is-4">Overall Co2 Emission</h2>
+                <div>
+                    <h1>{props.overallCo2.toFixed(4)} kgCo2 </h1>
                     <table class="charts-css area" style={{height: '200px', maxWidth: '400px', margin:' 0 auto'}}>
                     <caption> Overall Co2 Emission </caption>
                     <tbody>
                             <tr>
-                            <td style={style} > <span class="data"> {props.overallCo2.toFixed(4)} kg Co2 </span> </td>
+                            <td style={style} > <span class="data"> {props.overallCo2.toFixed(2)} kg Co2 </span> </td>
                             </tr>
                         </tbody>
                     </table>                
                 </div>
-                <footer class="card-footer">
-                    <a href="#" class="card-footer-item">Save</a>
-                    <a href="#" class="card-footer-item">Edit</a>
-                    <a href="#" class="card-footer-item">Delete</a>
-                 </footer>
+                
                 </div>
         </div>
     );
