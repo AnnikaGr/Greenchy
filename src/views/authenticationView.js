a22a39b26caa16f477d3e76d07abf86e6fcfd61f
Simon Langrieger <simon.langrieger@tum.de>
2022-05-03 14:55:49 +0200

    rewrote the Authentication component because the concerns were mixed

--- src/views/authenticationView.js
+++ src/views/authenticationView.js
@@ -1,11 +1,4 @@
-const AuthenticationView = {
-    props: ["isSignUp", "signIn", "signUp", "error"],
-    data() {
-        return {
-            credentials: {}
-        }
-    },
-    render() {
+function AuthenticationView(props) {
         return (
             <div class={"modal is-active"}>
                 <div class="modal-background"></div>
@@ -15,42 +8,35 @@ const AuthenticationView = {
                             <div class="field">
                                 <label class="label">Email</label>
                                 <div class="control">
-                                    <input class="input" type="email" onChange={(e) => this.credentials.email = e.target.value} placeholder="e.g. alex@example.com" />
+                                    <input class="input" type="email" onChange={(e) => props.onEmailChange(e.target.value)} placeholder="e.g. jane@doe.com" />
                                 </div>
                             </div>
 
-                            {this.isSignUp ? <div class="field">
+                            {props.isSignUp ? <div class="field">
                                 <label class="label">Full Name</label>
                                 <div class="control">
-                                    <input class="input" type="name" onChange={(e) => this.credentials.name = e.target.value} placeholder="Max Mustermann" />
+                                    <input class="input" type="name" onChange={(e) => props.onNameChange(e.target.value)} placeholder="Jane Doe" />
                                 </div>
                             </div> : false}
 
                             <div class="field">
                                 <label class="label">Password</label>
                                 <div class="control">
-                                    <input class="input" type="password" onChange={(e) => this.credentials.password = e.target.value} placeholder="********" />
+                                    <input class="input" type="password" onChange={(e) => props.onPasswordChange(e.target.value)} placeholder="********" />
                                 </div>
                             </div>
 
-                            {this.error.message ? <div class="has-text-danger">{this.error.message}</div> : ""}
+                            {props.error.message ? <div class="has-text-danger">{props.error.message}</div> : ""}
 
-                            <a class="button is-primary" onClick={() => {
-                                if (this.isSignUp) {
-                                    this.signUp(this.credentials)
-                                } else {
-                                    this.signIn(this.credentials)
-                                }
-                            }}>{this.isSignUp ? "Sign Up" : "Sign In"}</a>
+                            <a class="button is-primary" onClick={props.onSignInUp}>
+                                {props.isSignUp ? "Sign Up" : "Sign In"}
+                            </a>
                         </div>
                     </div>
                 </div>
                 <router-link to="/trips" class="modal-close is-large" aria-label="close"></router-link>
             </div>
         );
-    }
 }
-/*comment*/
-
 
 export default AuthenticationView;
