dfbe380b4aeeefd9748e67308b6263aa7ae98bcf
Annika Grothaus <annika.grothaus@gmx.de>
2022-05-05 13:31:15 +0200

    added license popup

--- src/presenters/welcomePagePresenter.js
+++ src/presenters/welcomePagePresenter.js
@@ -2,7 +2,21 @@ import WelcomePageView from "@/views/welcomePageView";
 
 const WelcomePage = {
   render() {
-    return <WelcomePageView />;
+    function onShowLicenseACB() {
+      let modal = document.getElementById("license-modal");
+      modal.classList.add("is-active");
+    }
+    function onHideLicenseACB() {
+      let modal = document.getElementById("license-modal");
+      modal.classList.remove("is-active");
+    }
+
+    return (
+      <WelcomePageView
+        onShowLicense={onShowLicenseACB}
+        onHideLicense={onHideLicenseACB}
+      />
+    );
   },
 };
 
--- src/views/welcomePageView.js
+++ src/views/welcomePageView.js
@@ -1,6 +1,18 @@
 import animateBike from "../views/animateBike.js";
 
 const WelcomePageView = {
+  props: {
+    onShowLicense: Function,
+    onHideLicense: Function,
+  },
+  methods: {
+    showLicense: function () {
+      this.onShowLicense();
+    },
+    hideLicense: function () {
+      this.onHideLicense();
+    },
+  },
   mounted() {
     animateBike();
   },
@@ -17,9 +29,51 @@ const WelcomePageView = {
           </div>
         </div>
 
+        <div class="modal" id="license-modal">
+          <div class="modal-background" onClick={this.hideLicense}></div>
+          <div class="modal-content">
+            <div class="box">
+              <p>License for the World Animation Copyright </p>
+              <p>
+                {" "}
+                (c) 2022 by Mariusz Dabrowski
+                (https://codepen.io/MarioD/pen/EvMNqE){" "}
+              </p>
+              <p>
+                Permission was granted, free of charge, to any person obtaining
+                a copy of the software and associated documentation files (the
+                "Software"), to deal in the Software without restriction,
+                including without limitation the rights to use, copy, modify,
+                merge, publish, distribute, sublicense, and/or sell copies of
+                the Software, and to permit persons to whom the Software is
+                furnished to do so, subject to the following conditions: The
+                above copyright notice and this permission notice shall be
+                included in all copies or substantial portions of the Software.
+                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+                EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+                OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+                NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+                HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+                WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+                FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+                OTHER DEALINGS IN THE SOFTWARE.
+              </p>
+            </div>
+          </div>
+          <button
+            class="modal-close is-large"
+            aria-label="close"
+            onClick={this.hideLicense}
+          ></button>
+        </div>
+
         <div class="planet-minigame column">
           <div class="post">
-            <button class="post__link" target="_blank">
+            <button
+              onClick={this.showLicense}
+              class="post__link"
+              target="_blank"
+            >
               License
             </button>
           </div>
