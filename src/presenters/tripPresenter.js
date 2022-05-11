2ed9f1241ec81e8eda44757cde0d9e41b680dc87
Annika Grothaus <annika.grothaus@gmx.de>
2022-05-11 19:28:30 +0200

    circle diagram responsiveness

--- src/presenters/tripPresenter.js
+++ src/presenters/tripPresenter.js
@@ -2,41 +2,47 @@ import Transportation from "./transportationPresenter";
 import TripSummary from "./tripSummaryPresenter";
 
 const Trip = {
-    props: ["userModel"],
-    render() {
-        const component = this;
-        const trip = this.userModel.tripsModel.getTrip(+this.$route.params.tripId)
+  props: ["userModel"],
+  render() {
+    const component = this;
+    const trip = this.userModel.tripsModel.getTrip(+this.$route.params.tripId);
 
-        function onTransportDeletionABC(transport, id) {
-            component.userModel.tripsModel.removeTransportation(id, transport);
-        }
-
-        function onSelectTransportACB(distance, passengers, modeOfTransport, co2) {
-            component.userModel.tripsModel.addTransportation(
-                trip.id,
-                distance,
-                passengers,
-                modeOfTransport,
-                co2
-            );
-        }
-
-        return (
-            <div class="page-wrapper container is-fluid">
-                <div class="box mt-5 has-text-centered">
-                    <h1 class="title">{trip.name}</h1>
-                </div>
-                <div class="columns">
-                    <div class="column">
-                        <Transportation onSelectTransport={onSelectTransportACB} trip={trip}/>
-                    </div>
-                    <div class="column is-one-half">
-                        <TripSummary onTransportDeletion={onTransportDeletionABC} trip={trip}/>
-                    </div>
-                </div>
-            </div>
-        );
+    function onTransportDeletionABC(transport, id) {
+      component.userModel.tripsModel.removeTransportation(id, transport);
     }
-}
 
-export default Trip
+    function onSelectTransportACB(distance, passengers, modeOfTransport, co2) {
+      component.userModel.tripsModel.addTransportation(
+        trip.id,
+        distance,
+        passengers,
+        modeOfTransport,
+        co2
+      );
+    }
+
+    return (
+      <div class="page-wrapper container is-fluid">
+        <div class="box mt-5 has-text-centered">
+          <h1 class="title">{trip.name}</h1>
+        </div>
+        <div class="columns is-desktop">
+          <div class="column">
+            <Transportation
+              onSelectTransport={onSelectTransportACB}
+              trip={trip}
+            />
+          </div>
+          <div class="column is-one-half mb-2">
+            <TripSummary
+              onTransportDeletion={onTransportDeletionABC}
+              trip={trip}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  },
+};
+
+export default Trip;
